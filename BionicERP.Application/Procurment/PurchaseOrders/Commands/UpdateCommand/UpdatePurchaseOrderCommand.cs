955f977d33919473c44a983a02ead85914cc5ac9
mikearaya <mikaelaraya12@gmail.com>
2019-12-05 13:28:40 +0300

    fixed purchase order update error

--- BionicERP.Application/Procurment/PurchaseOrders/Commands/UpdateCommand/UpdatePurchaseOrderCommand.cs
+++ BionicERP.Application/Procurment/PurchaseOrders/Commands/UpdateCommand/UpdatePurchaseOrderCommand.cs
@@ -6,10 +6,33 @@
  * @Last Modified Time: Aug 9, 2019 2:21 PM
  * @Description: Modify Here, Please  
  */
+using System;
+using System.Collections.Generic;
 using MediatR;
 
 namespace BionicERP.Application.Procurment.PurchaseOrders.Commands {
     public class UpdatePurchaseOrderCommand : IRequest {
 
+        public UpdatePurchaseOrderCommand () {
+            PurchaseOrderItems = new List<PurchaseOrderItem> ();
+        }
+
+        public uint Id { get; set; }
+        public uint VendorId { get; set; }
+        public string Status { get; set; }
+        public DateTime ExpectedDate { get; set; }
+        public DateTime? OrderedDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public float? Tax { get; set; }
+        public float? AdditionalFee { get; set; } = 0;
+        public float? Discount { get; set; }
+        public string OrderId { get; set; }
+        public DateTime? ArivalDate { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public string InvoiceId { get; set; }
+        public DateTime? InvoiceDate { get; set; }
+
+        public IEnumerable<PurchaseOrderItem> PurchaseOrderItems { get; set; }
     }
+
 }
--- BionicERP.Application/Procurment/PurchaseOrders/Commands/UpdateCommand/UpdatePurchaseOrderCommandHandler.cs
+++ BionicERP.Application/Procurment/PurchaseOrders/Commands/UpdateCommand/UpdatePurchaseOrderCommandHandler.cs
@@ -1,3 +1,4 @@
+using System;
 /*
  * @CreateTime: Aug 9, 2019 2:21 PM 
  * @Author:  Mikael Araya 
@@ -8,12 +9,42 @@
  */
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks;
+using AutoMapper;
+using BionicERP.Application.Exceptions;
+using BionicERP.Application.Interfaces;
+using BionicERP.Application.Inventory.StockBatchs.Commands;
+using BionicERP.Application.Inventory.StockBatchs.Models;
+using BionicERP.Domain.Procurment;
 using MediatR;
-
 namespace BionicERP.Application.Procurment.PurchaseOrders.Commands {
     public class UpdatePurchaseOrderCommandHandler : IRequestHandler<UpdatePurchaseOrderCommand, Unit> {
-        public Task<Unit> Handle (UpdatePurchaseOrderCommand request, CancellationToken cancellationToken) {
-            throw new System.NotImplementedException ();
+
+        private readonly IBionicERPDatabaseService _database;
+        private IMapper _Mapper;
+        public UpdatePurchaseOrderCommandHandler (IBionicERPDatabaseService database) {
+            _database = database;
+
+            _Mapper = new MapperConfiguration (x => {
+                x.CreateMap<UpdatePurchaseOrderCommand, PurchaseOrder> ();
+
+            }).CreateMapper ();
+
+        }
+        public async Task<Unit> Handle (UpdatePurchaseOrderCommand request, CancellationToken cancellationToken) {
+            PurchaseOrder updatedOrder = await _database.PurchaseOrder.FindAsync (request.Id);
+
+            if (updatedOrder == null) {
+                throw new NotFoundException ("Purchase order", request.Id);
+            }
+
+            _Mapper.Map (request, updatedOrder);
+
+            _database.PurchaseOrder.Update (updatedOrder);
+
+            await _database.SaveAsync ();
+
+            return Unit.Value;
         }
     }
 }
